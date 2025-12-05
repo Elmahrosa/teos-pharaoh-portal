68f9d217edc96c203cf0156795080bd715f57a8d
Ayman Seif <92527473+Aymanseif@users.noreply.github.com>
2025-12-05 19:48:12 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,28 @@
-docs: {
-  sidebarPath: require.resolve('./sidebars.ts'),
-  ...
-}
+// docusaurus.config.js
+module.exports = {
+  title: 'Teos Pharaoh Portal',
+  tagline: 'Official Documentation',
+  url: 'http://localhost:3000',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Elmahrosa',
+  projectName: 'Teos-Pharaoh-Portal',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'docs',
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.ts'),
+          editUrl: undefined,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
